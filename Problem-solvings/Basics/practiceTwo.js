4f5b8a5057eb2c5c50deb9761ae854a06076c30d
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-06-25 16:30:52 +0530

    Js practices for interview

--- Problem-solvings/Basics/practiceTwo.js
+++ Problem-solvings/Basics/practiceTwo.js
@@ -108,10 +108,6 @@ console.log('Outer scope :',c);//30
 
 
 //Scope level and mini hoisting:-
-
-
-
-
 function one(){
 const userName = "Yash Dandnaik"
 
@@ -135,3 +131,97 @@ if( website === "Youtube"){
     console.log("The website is secured");
 }
 }
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//25-06-24:-
+// THIS and arrow function in javascript | chai aur #javascript
+
+
+//'this' keyword refers to the object that the function is a property of. and the value of 'this' keyword will always depends on the object that is invoking the function.
+
+
+//Global object === Window object.
+
+//The simple way to understand the 'this' keyword is whenever the function is invoked check the object before the 'dot.`,and the value of 'this' keyword will always be the object before the dot.
+//If there is no object before the dot.the value of 'this' keyword will be the global object.
+
+
+
+
+const userInfo = {
+name:"Yash",
+userId:310398,
+welcomeMessage:function(){
+console.log(`Welcome ${this.name} to the website.`);
+console.log(this);
+}
+}
+userInfo.welcomeMessage();
+userInfo.name = "Siddhi";
+userInfo.welcomeMessage()
+
+console.log(`'this' keyword in the node environment which is in the global space ${this}`); 
+// { empty parentheses Object Object}
+
+//'this' keyword is in the Browser environment is access to the Window object.
+
+
+// function toCheck(){
+// console.log(this);
+// }
+
+// toCheck();
+
+// function toCheck(){
+//     let userName = "Yash"
+//     console.log(this.userName); //Undefined
+//     }
+    
+//     toCheck();
+
+
+// const toCheck = ()=> {
+//     let userName = "Yash"
+//     console.log(this); //{}
+//     }
+    
+//     toCheck();
+
+
+
+//explicit return  
+// const addNums = (num1,num2)=>{
+// return num1 + num2;
+// }
+
+// console.log(addNums(9,6)); //15
+
+
+// and implicit return
+const addNums = (num1,num2) => num1 + num2
+console.log(addNums(9,6));
+
+
+
+//Auto return:-
+
+const multiplyNums = (num1,num2) => (num1 * num2)
+console.log(multiplyNums(2,2)); //4
+
+
+
+
+///.IIFE (Immediately Invoked Function Expression):-
+//It is a function that runs as soon as it is defined, it is wrapped into the parentheses '()' The first is for delcaration and the second is '()' parentheses is for  'Execution or invokation'.
+
+(function sayMyName(){
+    //Named IIFE
+    let myName = "Yash"
+    console.log(`Hello my name is ${myName}`);
+})();//Hello my name is Yash
+
+((name)=>{
+    //Anonymous
+console.log(name);
+})("Animal"); //Animal
