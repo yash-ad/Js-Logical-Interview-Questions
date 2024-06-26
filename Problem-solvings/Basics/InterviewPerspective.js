ae420dd977bacdb6bd75beb2bfdbaa3ea98f7468
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-06-26 20:43:06 +0530

    Dsa problem solving questions

---
+++ Problem-solvings/Basics/InterviewPerspective.js
@@ -0,0 +1,20 @@
+//Logical Questions DSA:-
+
+
+//Q1.sum of all natural numbers from 1 to n:-
+ //lets say here the natural numbers are  sum of 1 to 5 : 15
+
+
+ //Lets declare a function called 'sumOfNaturalNumber()':-
+
+//  const sumOfNaturalNumber = (num)=> {
+//     let sum = 0;
+//     //Classic js forLoop.
+//     for(let i = 1; i<= num; i++){
+//         sum = sum + i;
+//     }
+//     return sum;
+//  }
+// console.log(sumOfNaturalNumber(5));//15
+// console.log(sumOfNaturalNumber(10));//55
+
